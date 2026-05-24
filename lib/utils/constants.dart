31b4bbdf86d6a1d47bbdf68e16ed374582b0a172
Bhaskar Pait <bhaskarpait44@gmail.com>
2026-05-25 01:54:43 +0530

    add address

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   // Change this to your PC's IP when working locally
-  static const String serverIp = '10.137.4.32'; 
+  static const String serverIp = '10.241.71.32'; 
   
   static const String baseUrl = 'http://$serverIp:5000/api';
   static const String mediaUrl = 'http://$serverIp:5000';
