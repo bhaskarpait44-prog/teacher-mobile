f1c3886d3e9cfdca4a8fb2d5ba418090f3fe04f7
bhaskarpait44-prog <bhaskarpait44@gmail.com>
2026-07-20 09:17:19 +0530

    fix: update default server IP to match current local IP

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,7 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiConstants {
-  static const String defaultServerIp = '10.226.18.216:5000';
+  static const String defaultServerIp = '10.97.34.216:5000';
   static String _currentServerIp = defaultServerIp;
 
   static Future<void> init() async {
